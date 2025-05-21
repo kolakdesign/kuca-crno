96618c267de348017be622ad2c9098c2c628b350
filipcro1 <filip.kolak@gmail.com>
2025-05-21 02:07:51 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,155 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dvorite-1",
+      "name": "Dvorište 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49753795312674853,
+          "pitch": 0.022421141879117812,
+          "rotation": 0,
+          "target": "1-dvorite-2"
+        },
+        {
+          "yaw": 0.5980217905263245,
+          "pitch": -0.002476431189496253,
+          "rotation": 4.71238898038469,
+          "target": "2-dvorite-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dvorite-2",
+      "name": "Dvorište 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19794298504795904,
+          "pitch": 0.07619576090128355,
+          "rotation": 0,
+          "target": "2-dvorite-3"
+        },
+        {
+          "yaw": 1.1602898514379962,
+          "pitch": 0.03033751265701845,
+          "rotation": 0,
+          "target": "0-dvorite-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dvorite-3",
+      "name": "Dvorište 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06067382528961218,
+          "pitch": -0.008482797072687731,
+          "rotation": 0,
+          "target": "1-dvorite-2"
+        },
+        {
+          "yaw": -2.8341409628989886,
+          "pitch": 0.0159822565332437,
+          "rotation": 14.137166941154074,
+          "target": "0-dvorite-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Kuća Crno",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
